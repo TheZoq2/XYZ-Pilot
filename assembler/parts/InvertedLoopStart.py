a09d1d7283dd89ba67e64472594188b4622a4b20
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-05-09 16:48:43 +0200

    Optimized inverted loop condition with new instruction BEQ, removing 1 instruction.

--- assembler/parts/InvertedLoopStart.py
+++ assembler/parts/InvertedLoopStart.py
@@ -4,7 +4,7 @@ from specification.instruction_spec import get_op_code, generate_instruction_byt
 
 class InvertedLoopStart(LoopStart):
     def occupied_addresses(self):
-        return 6
+        return 5
 
     def get_bytes(self):
         return \
@@ -12,5 +12,4 @@ class InvertedLoopStart(LoopStart):
             generate_instruction_bytes(get_op_code('LOAD'), LoopStart.USED_REGISTER_LHS, data=int(self.lhs, base=16)) + \
             generate_instruction_bytes(get_op_code('LOAD'), LoopStart.USED_REGISTER_RHS, data=int(self.rhs, base=16)) + \
             generate_instruction_bytes(get_op_code('CMP'), LoopStart.USED_REGISTER_LHS, LoopStart.USED_REGISTER_RHS) + \
-            generate_instruction_bytes(get_op_code('BNE'), data=self.address + 6) + \
-            generate_instruction_bytes(get_op_code('BRA'), data=self.end_address)
+            generate_instruction_bytes(get_op_code('BEQ'), data=self.end_address)
