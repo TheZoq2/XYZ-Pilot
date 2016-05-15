77fe9afbe438a55ad9ce9af5955ed9dd854247a0
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-05-15 23:25:32 +0200

    Moved debug parser to root of assembler to ease execution.

--- assembler/parse.py
+++ assembler/parse.py
@@ -53,7 +53,7 @@ def main():
     if len(argv) > 1:
         filename = argv[1]
     else:
-        filename = '../test.asm.out'
+        filename = 'test.asm.out'
 
     current_address = 0
     rows = []
