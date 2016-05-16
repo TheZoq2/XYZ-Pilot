d7273e6b1dfe8d26c98c1d9f9fa469ac5b6fb539
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-05-16 10:03:32 +0200

    Added remaining compare types to IF statements and WHILE loops.

--- assembler/parts/IfStart.py
+++ assembler/parts/IfStart.py
@@ -5,12 +5,19 @@ from specification.instruction_spec import get_op_code, generate_instruction_byt
 class IfStart(Part):
     USED_REGISTER_LHS = 0xE
     USED_REGISTER_RHS = 0xF
+    BRANCH_MAP = {
+        '=': 'BNE',
+        '!': 'BEQ',
+        '>': 'BLE',
+        '<': 'BGE'
+    }
 
-    def __init__(self, lhs, rhs):
+    def __init__(self, lhs, compare_type, rhs):
         self.lhs = lhs
         self.rhs = rhs
         self.lhs_register = IfStart.USED_REGISTER_LHS
         self.rhs_register = IfStart.USED_REGISTER_RHS
+        self.compare_instruction = IfStart.BRANCH_MAP.get(compare_type)
         self.end_address = None
 
     def occupied_addresses(self):
@@ -32,7 +39,7 @@ class IfStart(Part):
             instructions += generate_instruction_bytes(get_op_code('LOAD'), IfStart.USED_REGISTER_RHS, data=int(self.rhs, base=16))
 
         instructions += generate_instruction_bytes(get_op_code('CMP'), self.lhs_register, self.rhs_register)
-        instructions += generate_instruction_bytes(get_op_code('BNE'), data=self.end_address)
+        instructions += generate_instruction_bytes(get_op_code(self.compare_instruction), data=self.end_address)
 
         return instructions
 
--- assembler/parts/LoopStart.py
+++ assembler/parts/LoopStart.py
@@ -5,12 +5,19 @@ from specification.instruction_spec import get_op_code, generate_instruction_byt
 class LoopStart(Part):
     USED_REGISTER_LHS = 0xE
     USED_REGISTER_RHS = 0xF
+    BRANCH_MAP = {
+        '=': 'BNE',
+        '!': 'BEQ',
+        '>': 'BLE',
+        '<': 'BGE'
+    }
 
-    def __init__(self, lhs, rhs):
+    def __init__(self, lhs, compare_type, rhs):
         self.lhs = lhs
         self.rhs = rhs
         self.lhs_register = LoopStart.USED_REGISTER_LHS
         self.rhs_register = LoopStart.USED_REGISTER_RHS
+        self.compare_instruction = LoopStart.BRANCH_MAP.get(compare_type)
         self.address = None
         self.end_address = None
 
@@ -35,7 +42,7 @@ class LoopStart(Part):
                                                        data=int(self.rhs, base=16))
 
         instructions += generate_instruction_bytes(get_op_code('CMP'), self.lhs_register, self.rhs_register)
-        instructions += generate_instruction_bytes(get_op_code('BNE'), data=self.end_address)
+        instructions += generate_instruction_bytes(get_op_code(self.compare_instruction), data=self.end_address)
 
         return instructions
 
