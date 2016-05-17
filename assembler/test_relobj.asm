997799e3e47126ee4d0133abaad58e4555c90e6a
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-05-17 11:22:31 +0200

    Updated demo program for STOREOBJ.R.

--- assembler/test_relobj.asm
+++ assembler/test_relobj.asm
@@ -2,29 +2,31 @@ NOP
 
 i = 0
 num = A
+offset = 0
 
 LOAD 0 i
 LOAD 1 num
+LOAD 5 offset
 
-POSVEC = 001001000000000
-ADDVEC = 001001000000000
+POSVEC = 010001000000000
+ADDVEC = 010001000000000
 NULLVEC = 0000000000000000
 
 LOAD 2 POSVEC
 LOAD 3 ADDVEC
 LOAD 4 NULLVEC
 
-ALIAS 5 offset
-	
-WHILE i <= num
+# Following statement logic is reveresed (i <= num)
+WHILE num <= i
   VECADD POSVEC POSVEC ADDVEC
   STOREOBJ.R POSVEC offset 0
   STOREOBJ.R NULLVEC offset 1
   STOREOBJ.R NULLVEC offset 2
   STOREOBJ.R NULLVEC offset 3
-  ADDI offset offset 1
+  ADDI offset offset 4
 	
-  SUBI i i 1
+  ADDI i i 1
+ENDWHILE
 
 LOOP:
   BRA LOOP
