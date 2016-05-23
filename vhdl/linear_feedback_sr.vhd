0ef5256a3a9fdd97109936d0dd18d71c1d7f4e35
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-05-23 09:19:57 +0200

    Simplified LFSR code.

--- vhdl/linear_feedback_sr.vhd
+++ vhdl/linear_feedback_sr.vhd
@@ -8,30 +8,33 @@ use work.Vector;
 entity LinearFeedbackSR is
 port (
         clk : in std_logic;
-        next_bit_out : out Vector.InMemory_t;
         data : out Vector.InMemory_t
     );
 end entity;
 
 architecture Behavioral of LinearFeedbackSR is
   signal state : Vector.InMemory_t := x"040815162342_2760";  -- Vino Tinto Español!
-  signal next_bit : Vector.InMemory_t := (others => '0');
-  signal action : std_logic := '0';
+
+  signal state_lsr1 : Vector.InMemory_t;
+  signal state_lsr3 : Vector.InMemory_t;
+  signal state_lsr4 : Vector.InMemory_t;
+  signal xor_result : Vector.InMemory_t;
+  signal next_bit : std_logic;
 
 begin
-
   data <= state;
-  next_bit_out <= next_bit;
 
+  state_lsr1 <= "0"    & state(Vector.MEMORY_SIZE - 1 downto 1);
+  state_lsr3 <= "000"  & state(Vector.MEMORY_SIZE - 3 downto 1);
+  state_lsr4 <= "0000" & state(Vector.MEMORY_SIZE - 4 downto 1);
+
+  xor_result <= (state xor state_lsr1 xor state_lsr3 xor state_lsr4);
+  next_bit <= xor_result(0);
+  
   process(clk)
     begin
       if rising_edge(clk) then
-        action <= not action;
-        if action='0' then
-          next_bit <= (((state XOR ("0" & state(Vector.MEMORY_SIZE - 1 downto 1))) XOR ("000" & state(Vector.MEMORY_SIZE - 3 downto 1))) XOR ("0000" & state(Vector.MEMORY_SIZE - 4 downto 1))) and x"0000000000000001";
-        else
-          state <= next_bit(0) & state(Vector.MEMORY_SIZE - 1 downto 1);
-        end if;
+        state <= next_bit & state(Vector.MEMORY_SIZE - 1 downto 1);
       end if;
     end process;
 
