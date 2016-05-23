59aab04a8d2ecd6b260424d1a6d0e29d7d2bc509
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-05-23 09:54:54 +0200

    Removed next_bit_out from LFSR test bench.

--- vhdl/lf_tb.vhd
+++ vhdl/lf_tb.vhd
@@ -12,18 +12,15 @@ architecture Behavioral of lf_tb is
 
 component LinearFeedbackSR is
 	port (clk		: in std_logic;								-- System clock
-              next_bit_out : out Vector.InMemory_t; 
               data : out Vector.InMemory_t); 
 end component;
 	
 signal clk : std_logic := '0';
 signal data : Vector.InMemory_t;
-signal next_bit_out : Vector.InMemory_t;
 
 begin
 	uut: LinearFeedbackSR port map(
 	clk=>clk,
-        next_bit_out=>next_bit_out,
         data=>data);
 
 	-- clk 100 MHz
