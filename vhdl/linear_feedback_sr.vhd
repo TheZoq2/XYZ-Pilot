027d8fa01b9d8708eafdcc2c251c4118b991741b
Patrik Sletmo <patrik.sletmo@gmail.com>
2016-05-23 09:40:08 +0200

    Added some comments.

--- vhdl/linear_feedback_sr.vhd
+++ vhdl/linear_feedback_sr.vhd
@@ -1,3 +1,6 @@
+-- Implements a standard 64-bit Linear-feedback shift register,
+-- see https://en.wikipedia.org/wiki/Linear-feedback_shift_register for details.
+
 library IEEE;
 
 use IEEE.numeric_std.all;
@@ -7,17 +10,24 @@ use work.Vector;
 
 entity LinearFeedbackSR is
 port (
-        clk : in std_logic;
-        data : out Vector.InMemory_t
-    );
+  -- System clock
+  clk : in std_logic;
+
+  -- Current register data
+  data : out Vector.InMemory_t
+  );
 end entity;
 
 architecture Behavioral of LinearFeedbackSR is
+  -- Pre-seeded (non-zero) state of the LFSR.
   signal state : Vector.InMemory_t := x"040815162342_2760";  -- Vino Tinto Español!
 
+  -- Signals for state shifted right 1, 3, and 4 times.
   signal state_lsr1 : Vector.InMemory_t;
   signal state_lsr3 : Vector.InMemory_t;
   signal state_lsr4 : Vector.InMemory_t;
+
+  -- Result from LFSR xor and final output bit.
   signal xor_result : Vector.InMemory_t;
   signal next_bit : std_logic;
 
@@ -34,6 +44,8 @@ begin
   process(clk)
     begin
       if rising_edge(clk) then
+        -- Modify state every clock with the new bit to ensure random data every
+        -- time the CPU fetches information.
         state <= next_bit & state(Vector.MEMORY_SIZE - 1 downto 1);
       end if;
     end process;
